76e7fca1cdf4614644cccb5f987fb92da2d85cf7
Jhonatan-Ordonez <jhoeduardonez@gmail.com>
2023-12-04 19:49:16 -0500

    Added logo to nav and aligned ul list

--- siga-php/nav.php
+++ siga-php/nav.php
@@ -1,5 +1,7 @@
 <nav>
+
     <ul>
+    <img width="100px" src="../resources/images/logowhite.ico">
         <li><a href="">Inicio</a> </li>
         <li><a href="">Docentes</a> </li>
         <li><a href="">Alumnos</a> </li>
